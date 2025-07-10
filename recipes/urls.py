0f35d8cbcca3f72ed331f8016e012779458a522e
rodrigo <rodrigog3wconcept@gmail.com>
2025-07-09 21:16:10 -0300

    Add font awesome

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home ), # HOME
-    path('sobre/', sobre ), # /Sobre/
-    path('contato/', contato ), # /Contato/
-
+ 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,9 +6,4 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/home.html')
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
 
