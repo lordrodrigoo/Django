4423d1f40a42c18b615ee6f36720685d07d0c746
rodrigo <rodrigog3wconcept@gmail.com>
2025-07-26 16:26:13 -0300

    Created a base class for the tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,7 +1,11 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()
 
 
 class RecipeViewsTest(TestCase):
@@ -23,31 +27,6 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here</h1>',
             response.content.decode('utf-8')
         )
-
-    def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-        
 
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
