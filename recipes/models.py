c6d46e69ad479a7d808dfbeaaed0784c06ffb1de
rodrigo <rodrigog3wconcept@gmail.com>
2025-07-26 16:11:47 -0300

    Use self.client of Django for simulated the cliente on tests and modifying fixed models

--- recipes/models.py
+++ recipes/models.py
@@ -16,7 +16,7 @@ class Recipe(models.Model):
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
-    servins = models.IntegerField()
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField() 
     preparation_steps_is_html = models.BooleanField(default=False)
