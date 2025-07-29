b2ca1d06e023b2770dc3a8808ec328ec3db8a0b7
rodrigo <rodrigog3wconcept@gmail.com>
2025-07-29 17:00:06 -0300

    raising error 404 if the term search no to send

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -136,3 +136,6 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:search'))
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
         
+    def test_recipe_search_reaises_404_if_no_search_term(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertEqual(response.status_code, 404)
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import Http404
 from utils.recipes.factory import make_recipe
+
 from recipes.models import Recipe
 
 # Create your views here.
@@ -33,4 +35,9 @@ def recipe(request, id):
     })
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+    
     return render(request, 'recipes/pages/search.html')
