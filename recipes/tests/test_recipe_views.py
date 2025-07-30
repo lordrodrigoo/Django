7779e875d109c1b2730eb271e10851e8bdf1f14e
rodrigo <rodrigog3wconcept@gmail.com>
2025-07-30 16:42:28 -0300

    test for assurance that search_term be scaped to security

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -137,5 +137,14 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
         
     def test_recipe_search_reaises_404_if_no_search_term(self):
-        response = self.client.get(reverse('recipes:search'))
+        url = reverse('recipes:search')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+         url = reverse('recipes:search') + '?q=Teste'
+         response = self.client.get(url)
+         self.assertIn(
+             'Search for &quot;Teste&quot; ',
+             response.content.decode('utf-8')
+         )
