443b55a373dffe329b43f46e6bc10971bfea16df
rodrigo <rodrigog3wconcept@gmail.com>
2025-09-15 18:58:01 -0300

    moving PER_PAGE to environment variables in Django (.env)

--- recipes/views.py
+++ recipes/views.py
@@ -5,12 +5,15 @@ from recipes.models import Recipe
 from django.core.paginator import Paginator
 from utils.pagination import make_pagination
 
+import os
 # Create your views here.
+PER_PAGE = os.environ.get('PER_PAGE',)
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, per_page=9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE )
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -25,7 +28,7 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, per_page=9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -55,7 +58,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, per_page=9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
