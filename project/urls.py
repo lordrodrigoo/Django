1a35b4de16b32084ad49d301514e2d98f09bb5b6
rodrigo <rodrigog3wconcept@gmail.com>
2025-09-16 16:59:40 -0300

    Creating new app authors for register of the Users

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls')), # /
+    path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
+
     
 ]
 
