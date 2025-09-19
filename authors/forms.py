dbf38e8c6099c270dd1e488e58c3f49b35677226
rodrigo <rodrigog3wconcept@gmail.com>
2025-09-19 00:54:01 -0300

    Testing others fields, password, confirm password, empty fields etc .

--- authors/forms.py
+++ authors/forms.py
@@ -18,8 +18,8 @@ def strong_password(password):
 
     if not regex.match(password):
         raise ValidationError((
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. The length should be'
             'at least 8 characters.'
         ),
             code='invalid'
@@ -36,9 +36,36 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Type your password')
         add_placeholder(self.fields['ConfirmPassword'], 'Confirm your password')
 
+    username = forms.CharField(
+        label='Username',
+        help_text=(
+            'Username must have letters, numbers or one of those @.+-_. '
+            'The length should be between 4 and 150 characters.'
+        ),
+        error_messages={
+            'required': 'This field must not be empty',
+            'min_length': 'Username must have at least 4 characters',
+            'max_length': 'Username must have less than 150 characters',
+        },
+        min_length=4, max_length=150
+    )
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required.'},
+        label='E-mail',
+        help_text='The e-mail must be valid.'
+    )
 
     password = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
@@ -52,9 +79,11 @@ class RegisterForm(forms.ModelForm):
         label='Password'
     )
     ConfirmPassword = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
-        label='Confirm Password'
+        label='Confirm Password',
+        error_messages={
+            'required': 'Confirm your password'
+        },
     )
 
     class Meta:
