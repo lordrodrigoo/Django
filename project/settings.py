1869f68fa97c1189700ccc1d0e3a75ca189e383a
rodrigo <rodrigog3wconcept@gmail.com>
2025-10-14 16:27:58 -0300

    Fix ALLOWED_HOST AND CSRF_TRUSTED_ORIGINS

--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,7 @@ import os
 
 from pathlib import Path
 from django.contrib.messages import constants
+from utils.enviroment import parse_comma_str_to_list, get_env_variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,10 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',    
-    'localhost',       
-]
+ALLOWED_HOSTS = parse_comma_str_to_list(get_env_variable('ALLOWED_HOSTS'))
+CSRF_TRUSTED_ORIGINS = parse_comma_str_to_list(get_env_variable('CSRF_TRUSTED_ORIGINS'))
 
 
 # Application definition
