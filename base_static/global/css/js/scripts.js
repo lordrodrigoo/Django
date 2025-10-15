d8359cc3f591412f63e3757594de0455f5bf3552
rodrigo <rodrigog3wconcept@gmail.com>
2025-10-15 17:55:13 -0300

    Add js for menu

--- base_static/global/css/js/scripts.js
+++ base_static/global/css/js/scripts.js
@@ -1,16 +1,44 @@
-function my_scope() {
-    const forms = document.querySelectorAll('.form-delete');
+(() => {
+  const forms = document.querySelectorAll('.form-delete');
 
-    for (const form of forms) {
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
+  for (const form of forms) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
 
-            const confirmed = confirm('Are you sure you want to delete this recipe?');
-            if (confirmed) {
-                form.submit();
-            }
-        });
-    }
-}
+      const confirmed = confirm('Are you sure?');
 
-my_scope();
+      if (confirmed) {
+        form.submit();
+      }
+    });
+  }
+})();
+
+(() => {
+  const buttonCloseMenu = document.querySelector('.button-close-menu');
+  const buttonShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
+
+  const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    menuContainer.classList.add(menuHiddenClass);
+  };
+
+  const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    menuContainer.classList.remove(menuHiddenClass);
+  };
+
+  if (buttonCloseMenu) {
+    buttonCloseMenu.removeEventListener('click', closeMenu);
+    buttonCloseMenu.addEventListener('click', closeMenu);
+  }
+
+  if (buttonShowMenu) {
+    buttonShowMenu.removeEventListener('click', showMenu);
+    buttonShowMenu.addEventListener('click', showMenu);
+  }
+})();
