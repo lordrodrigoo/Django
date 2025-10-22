566b162ee1369862793b4ed2c2f2343c21ccb885
rodrigo <rodrigog3wconcept@gmail.com>
2025-10-22 18:36:10 -0300

    creating permitions

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,11 +1,15 @@
 from django.shortcuts import get_object_or_404
+from tag.models import Tag
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from tag.models import Tag
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import status
 
+
+from ..permissions import IsOwner
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 import os
@@ -19,6 +23,8 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -29,6 +35,47 @@ class RecipeAPIv2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+    
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        obj = get_object_or_404(self.get_queryset(), pk=pk)
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+    
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(),]
+        return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+        
+
+
+    def partial_update(self, request, *args, **kwargs):
+        recipe = self.get_object()
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            partial=True,
+            context={'request': request},
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 @api_view()
 def tag_api_detail(request, pk):
