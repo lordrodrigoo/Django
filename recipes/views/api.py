1bc64aeddbab1bb9d97e286d3a63fe4deda603e8
rodrigo <rodrigog3wconcept@gmail.com>
2025-10-22 16:37:01 -0300

    Router

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -20,6 +20,15 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
 
 @api_view()
 def tag_api_detail(request, pk):
